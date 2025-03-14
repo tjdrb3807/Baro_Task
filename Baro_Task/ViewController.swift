27ef8d3794fe21ef481d5a22a4f01824bf9032ee
Jeon_Seong_gyu <junsung3807@naver.com>
2025-03-14 17:07:23 +0900

    [Feat] 프로젝트 생성

---
+++ Baro_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baro_Task
+//
+//  Created by 전성규 on 3/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
