79271b3dd12f39113e0b534df6ddb2546db188c5
Jeon_Seong_gyu <junsung3807@naver.com>
2025-03-14 19:07:12 +0900

    [Refactor] BaseVC final 키워드 제거

--- Baro_Task/Presentation/Common/BaseViewController.swift
+++ Baro_Task/Presentation/Common/BaseViewController.swift
@@ -10,8 +10,8 @@ import RxSwift
 
 /// 모든 ViewController가 상속받는 기본 뷰 컨트롤러
 /// - 공통적으로 필요한 UI 설정 및 바인딩을 처리하기 위한 베이스 클래스
-final class BaseViewController: UIViewController {
-    private let disposeBag = DisposeBag()
+class BaseViewController: UIViewController {
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
