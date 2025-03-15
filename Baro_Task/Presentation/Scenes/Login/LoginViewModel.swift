59945ac2fa7426388c1a0521fcbce6babed7635b
Jeon_Seong_gyu <junsung3807@naver.com>
2025-03-15 21:24:41 +0900

    [Feat] LoginViewModel 구현

--- Baro_Task/Presentation/Scenes/Login/LoginViewModel.swift
+++ Baro_Task/Presentation/Scenes/Login/LoginViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+
+final class LoginViewModel: ViewModelType {
+    struct Input {
+        let startButtonTap: Observable<Void>
+    }
+    
+    struct Output {
+        let navigateToScreen: Observable<Bool>
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        let navigateToScreen = input.startButtonTap
+            .map { _ in
+                // 자동 로그인 여부 확인
+                let isLoggedIn = UserDefaults.standard.bool(forKey: "isLoggedIn")
+                let userID = UserDefaults.standard.string(forKey: "loggedInUserID")
+                
+                return isLoggedIn && userID != nil
+            }
+            
+        return Output(navigateToScreen: navigateToScreen)
+    }
+}
