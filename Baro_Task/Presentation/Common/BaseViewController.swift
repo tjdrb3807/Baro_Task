30a1ca0c85132a18cbb9fe6ab96481fd764aeaf1
Jeon_Seong_gyu <junsung3807@naver.com>
2025-03-15 22:05:39 +0900

    [Feat] 키보드 비활성화 로직 구현

--- Baro_Task/Presentation/Common/BaseViewController.swift
+++ Baro_Task/Presentation/Common/BaseViewController.swift
@@ -11,6 +11,9 @@ import RxSwift
 /// 모든 ViewController가 상속받는 기본 뷰 컨트롤러
 /// - 공통적으로 필요한 UI 설정 및 바인딩을 처리하기 위한 베이스 클래스
 class BaseViewController: UIViewController {
+    /// 키보드가 나타났을 때 탭 제스처로 키보드를 내리는 기능 활성화 여부
+    var shouldEnableKeyboardDismiss: Bool { false }
+    
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
@@ -19,6 +22,8 @@ class BaseViewController: UIViewController {
         self.bind()
         self.configureViews()
         self.configureConstraints()
+        
+        if shouldEnableKeyboardDismiss { bindKeyboardDismiss() }
     }
     
     /// ViewModel과 데이터 바인딩을 설정하는 메서드
@@ -29,4 +34,20 @@ class BaseViewController: UIViewController {
     
     /// AutoLayout을 설정하는 메서드
     func configureConstraints() { }
+}
+
+// MARK: - 키보드 해제 기능 (화면 탭 시 키보드 내리기)
+extension BaseViewController {
+    /// 화면을 탭하면 키보드를 내리는 기능을 설정하는 메서드
+    /// - shouldEnableKeyboardDismiss가 true인 경우에만 활성화됨
+    private func bindKeyboardDismiss() {
+        let tapGesture = UITapGestureRecognizer()
+        view.addGestureRecognizer(tapGesture)
+        
+        tapGesture.rx.event
+            .withUnretained(self)
+            .bind(onNext: { vc, _ in
+                vc.view.endEditing(true)
+            }).disposed(by: disposeBag)
+    }
 }
