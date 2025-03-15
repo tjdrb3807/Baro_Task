7fb0e528189918fe16aac9836f1c8d5954d81126
Jeon_Seong_gyu <junsung3807@naver.com>
2025-03-15 21:22:04 +0900

    [Feat] UILabel 마지막 글자(에스터리스크) 색상 설정 메서드 구현

--- Baro_Task/Presentation/Extensions/UILabel+Extensions.swift
+++ Baro_Task/Presentation/Extensions/UILabel+Extensions.swift
@@ -5,4 +5,17 @@
 //  Created by 전성규 on 3/15/25.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    func setTitleLabelColor() {
+        guard let text = self.text, let _ = text.last else { return }
+        
+        let attributedString = NSMutableAttributedString(string: text)
+        let lastCharRange = NSRange(location: text.count - 1, length: 1)
+        
+        attributedString.addAttribute(.foregroundColor, value: UIColor.red, range: lastCharRange)
+        
+        self.attributedText = attributedString
+    }
+}
